64f4357eaba505d4dd5a440482596f9663f3a82d
Nguyễn Văn An <nva6112002@gmail.com>
2024-05-23 17:13:54 +0700

    Fix: get revenue this current - rename variable

--- src/main/java/com/hcmute/shopfee/dto/response/GetRevenueCurrentDateResponse.java
+++ src/main/java/com/hcmute/shopfee/dto/response/GetRevenueCurrentDateResponse.java
@@ -7,11 +7,11 @@ import lombok.Data;
 public class GetRevenueCurrentDateResponse {
     private double revenueByToday;
     private double revenueByThisMonth;
-    private double revenue;
+    private double revenueByThisYear;
 
     public static GetRevenueCurrentDateResponse fromRevenueQueryDto(GetRevenueQueryDto revenueQueryDto) {
         GetRevenueCurrentDateResponse data = new GetRevenueCurrentDateResponse();
-        data.setRevenue(revenueQueryDto.getRevenueByThisYear() != null ? revenueQueryDto.getRevenueByThisYear() : 0);
+        data.setRevenueByThisYear(revenueQueryDto.getRevenueByThisYear() != null ? revenueQueryDto.getRevenueByThisYear() : 0);
         data.setRevenueByToday(revenueQueryDto.getRevenueByToday() != null ? revenueQueryDto.getRevenueByToday() : 0);
         data.setRevenueByThisMonth(revenueQueryDto.getRevenueByThisMonth() != null ? revenueQueryDto.getRevenueByThisMonth() : 0);
         return data;
